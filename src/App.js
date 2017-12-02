3f2b1613ac2919936856bd8ddc28a41455087673
B-eck37 <brent.eckert7@gmail.com>
2017-12-01 17:48:33 -0700

    Step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import './index.css';
-import TopicBrowser from './components/TopicBrowser/TopicBrowser'
+import React, {Component} from 'react';
+import './index.css'
+import TopicBrowser from './components/TopicBrowser/TopicBrowser.js'
 
 class App extends Component {
   render() {
     return (
+      <div>
     <TopicBrowser />
+    </div>
     )
   }
 }
